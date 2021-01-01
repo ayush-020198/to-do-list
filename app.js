e1f8b8698707c0197e7ba258edffce61cb9aceee
ayush-020198 <ayush.shrivastavaujn@gmail.com>
2021-01-01 17:50:24 +0530

    Adding Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,7 +9,23 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-let items = ["Eat Food", "Code"];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Hit the + button to add new item"
+})
+
+const item2 = new Item({
+    name: "Hit the - button to delete new item"
+})
+
+const defaultItems = [item1, item2];
 
 app.get('/', function(req,res){
     let today = new Date();
@@ -20,15 +36,48 @@ app.get('/', function(req,res){
     };
 
     let day = today.toLocaleDateString("en-US", options)
-
-    res.render("list", {kindOfDay:day, newItems: items});
+    
+    Item.find({}, function(err, foundItems){
+        if(foundItems.length===0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    console.log("Successfully saved items")
+                }
+            });
+        }
+        else {
+            res.render("list", {kindOfDay:day, newItems: foundItems});
+        }  
+    });
 });
 
 app.post('/', function(req, res){
-    let item = req.body.newItem;
-    items.push(item);
+    const itemName = req.body.newItem;
+    
+    const item = new Item({
+        name: itemName
+    })
+
+    item.save();
     res.redirect('/');
 });
+
+app.post('/delete', function(req, res){
+    const checkedItemID = req.body.itemChecked;
+
+    Item.findByIdAndRemove(checkedItemID, function(err){
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log("Successfully Deleted");
+        }
+    });
+    res.redirect('/');
+})
 
 app.listen(3000, function(){
     console.log('Server is running on port 3000')
